49325e2efbe4a1089e6bbf07281a3c17554e2375
Aksh Bansal <akshmark@gmail.com>
2021-06-22 22:33:46 +0530

    hashmap working

---
+++ dataStructures/String.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	printf("a to num: %d", (int)'a');	
+	return  0;
+}
--- dataStructures/hashmap.c
+++ dataStructures/hashmap.c
@@ -4,31 +4,25 @@
 
 const long MAP_SIZE = 10000000;
 typedef long long ll;
-// typedef struct Hentry Hentry;
 
 typedef struct Hentry{
 	char* key;
 	char* value;
 	Hentry* next;
-};
-// struct Hentry{
-// 	int key;
-// 	int val;
-// 	Hentry* next;
-// };
+} Hentry;
 
 typedef struct{
 	Hentry **arr;
 }HashMap;
 
-HashMap new_HashMap(HashMap *map){
-	// map->arr = (typeof(map->arr))malloc(MAP_SIZE* sizeof(typeof(map->arr)));
-	map->arr = (Hentry**)malloc(MAP_SIZE*sizeof(Hentry**));
+HashMap new_HashMap(){
+	HashMap map;
+	map.arr = (Hentry**)malloc(MAP_SIZE*sizeof(Hentry**));
 
 	for(int i=0;i<MAP_SIZE;i++){
-		map->arr[i] = NULL;
+		map.arr[i] = NULL;
 	}
-	return *map;
+	return map;
 }
 
 // long num_hash(long n){
@@ -55,24 +49,23 @@ ll hash(char *str){
 }
 
 void map_add(HashMap *map, char *key, char *val){
-	Hentry e;
-	e.key = key;
-	e.value = val;
-	e.next = NULL;
+	Hentry* e = (Hentry*)malloc(sizeof(Hentry*));
+	e->key = key;
+	e->value = val;
+	e->next = NULL;
 
 	ll hash_key;
 	hash_key = hash(key);
 
 	if(!map->arr[hash_key]){
-		map->arr[hash_key] = &e;
-		printf("%s --%lld-- %s\n", map->arr[hash_key]->key,hash_key, map->arr[hash_key]->value);
+		map->arr[hash_key] = e;
 	}
 	else{
 		Hentry* temp = map->arr[hash_key];
 		while(temp->next){
 			temp = temp->next;
 		}
-		temp->next = &e;
+		temp->next = e;
 	}
 }
 bool map_containsKey(HashMap *map, char *key){
@@ -81,12 +74,10 @@ bool map_containsKey(HashMap *map, char *key){
 		return false;
 	}
 	Hentry* temp = map->arr[hash_key];
-	printf("%s --%lld-- %s\n", map->arr[hash_key]->key,hash_key, map->arr[hash_key]->value);
 	
 	while(temp && temp->key!=key){
 		temp = temp->next;
 	}
-	// printf("safe\n");
 	if(!temp || temp->key!=key){
 		return false;
 	}
@@ -99,7 +90,6 @@ char *map_get(HashMap *map, char *key){
 		return "";
 	}
 	Hentry* temp = map->arr[hash_key];
-	// printf("%s --%lld-- %s\n", map->arr[hash_key]->key,hash_key, map->arr[hash_key]->val);
 	while(temp && temp->key!=key){
 		temp = temp->next;
 	}
@@ -110,9 +100,13 @@ char *map_get(HashMap *map, char *key){
 }
 
 int main(){
-	HashMap map = new_HashMap(&map);
+	HashMap map = new_HashMap();
 	map_add(&map, "123", "please no err");
+	map_add(&map, "go", "ready");
 	printf("%s\n", map_containsKey(&map, "123")?"true":"false");
-	// printf("%s", map_get(&map, "123"));
+	printf("%s\n", map_get(&map, "123"));
+	printf("%s\n", map_containsKey(&map, "13")?"true":"false");
+	printf("%s\n", map_containsKey(&map, "go")?"true":"false");
+	printf("%s\n", map_get(&map, "go"));
 	return 0;
 }
